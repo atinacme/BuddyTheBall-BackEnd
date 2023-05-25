8e9e09e6334621d44582dd5ad6743893ee6d625e
guptaatin <guptaatin1997@gmail.com>
2023-05-25 15:48:19 +0530

    changes commited

--- app/controllers/photos.controller.js
+++ app/controllers/photos.controller.js
@@ -223,26 +223,53 @@ const getParticularCustomerPhotos = async (req, res) => {
 
 const getAwardPhotos = async (req, res) => {
     try {
-        var fileInfos = [];
-        var photos = await Photos.find({ upload_for: 'award' });
+        await mongoClient.connect();
 
-        if ((photos.length) === 0) {
-            return res.status(500).send({
-                message: "No files found!",
-            });
-        }
-
-        photos.forEach((doc) => {
-            fileInfos.push({
-                _id: doc._id,
-                photo_id: doc.photo_id,
-                originalname: doc.originalname,
-                name: doc.originalname.replace(".png", ""),
-                url: baseUrl + doc.filename,
-                messages: doc.messages
-            });
+        const database = mongoClient.db(dbConfig.database);
+        const bucket = new GridFSBucket(database, {
+            bucketName: dbConfig.imgBucket,
         });
-        return res.status(200).send(fileInfos);
+        bucket.find().toArray((err, files) => {
+            if (!files || files.length == 0) {
+                return res.status(200).json({
+                    success: false,
+                    message: "No files available"
+                })
+            }
+
+            files.map(file => {
+                if (file.contentType === "image/jpeg" || file.contentType === "image/png" || file.contentType === "image/svg+xml") {
+                    file.isImage = true
+                } else {
+                    file.isImage = false
+                }
+            })
+            res.status(200).json({
+                success: true,
+                files
+            })
+        })
+        // let downloadStream = bucket.openDownloadStreamByName();
+        // var fileInfos = [];
+        // var photos = await Photos.find({ upload_for: 'award' });
+
+        // if ((photos.length) === 0) {
+        //     return res.status(500).send({
+        //         message: "No files found!",
+        //     });
+        // }
+
+        // photos.forEach((doc) => {
+        //     fileInfos.push({
+        //         _id: doc._id,
+        //         photo_id: doc.photo_id,
+        //         originalname: doc.originalname,
+        //         name: doc.originalname.replace(".png", ""),
+        //         url: baseUrl + doc.filename,
+        //         messages: doc.messages
+        //     });
+        // });
+        // return res.status(200).send(fileInfos);
     } catch (error) {
         return res.status(500).send({
             message: error.message,
