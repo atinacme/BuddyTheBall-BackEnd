d4a24d975777c4234ce53a92d5f89af736d4049b
guptaatin <guptaatin1997@gmail.com>
2023-09-01 11:52:13 +0530

    changes commited in databse

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+
 const db = require("./app/models");
 const Role = db.role;
 const Schedule = db.schedule;
